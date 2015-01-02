d460fd600157933a95fba818d182c3d9cba5c575
Adam Fallon <A.Fallon@kainos.com>
2015-01-02 21:23:11 +0000

    Initial Commit

---
+++ Note/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Note
+//
+//  Created by Adam Fallon on 02/01/2015.
+//  Copyright (c) 2015 com.dotly.note. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Note/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Note
+//
+//  Created by Adam Fallon on 02/01/2015.
+//  Copyright (c) 2015 com.dotly.note. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
